cf6a8d9f0d413493082ee4bd5e36b839fbd735e9
Bruno Faviero <bfaviero@mit.edu>
2015-03-01 14:58:16 -0600

    x

--- kiwi/wolfram.py
+++ kiwi/wolfram.py
@@ -26,7 +26,7 @@ class Wolfram():
 
 		print "Searching for solution for equation:"
 		print "\t %s" % expr
-		res = self.client.query("(%s) = 0" % expr)
+		res = self.client.query("(%s) = x" % expr)
 
 		possible_titles = [
 			"Solutions", 
